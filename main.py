99d0cac3c653b8cea7279f72928763d2c6de2cd8
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-05-23 12:28:14 +0900

    점수를 AWS 데이터베이스에 연결하도록 수정

--- main.py
+++ main.py
@@ -1,24 +1,60 @@
-import json
+import asyncore
+import pickle
 import socket
+import pymysql
+
+score_outgoing = []
+buffer_size = 512
+
+client = pymysql.connect(
+    user="jaram", password="Qoswlfdlsnrn", database="game",
+    host="game-server.cfxyvd6mgspo.ap-northeast-2.rds.amazonaws.com", port=3306)
 
 
-def init_server():
-    server_socket = socket.socket()
-    server_socket.bind(('127.0.0.1', 8080))
-    server_socket.listen(10)
-    while True:
-        connection, address = server_socket.accept()
-        while True:
-            received_data = connection.recv(1024).decode()
+class MainServer(asyncore.dispatcher):
+    def __init__(self, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind(('', port))
+        self.listen(10)
+
+    def handle_accept(self) -> None:
+        connection, address = self.accept()
+        ScoreServer(connection)
+
+
+class ScoreServer(asyncore.dispatcher_with_send):
+    def handle_read(self) -> None:
+        received_data = self.recv(buffer_size)
+        if received_data:
+            received_data = dict(pickle.loads(received_data))
+            if 'hostname' in received_data.keys() and 'stage' in received_data.keys():
+                score_outgoing.append(self.socket)
+                self.send_score(received_data)
+        else:
+            self.close()
+
+    @staticmethod
+    def send_score(data: dict):
+        cursor = client.cursor()
+        cursor.execute("insert into game.score(hostname, score) values (%s, %s)", (data['hostname'], data['stage']))
+        cursor.execute("select hostname, score from game.score order by score desc")
+
+        send_data = []
+        for hostname, score in cursor.fetchall():
+            send_data.append({"hostname": hostname, "stage": score})
+        remove = []
+        for i in score_outgoing:
             try:
-                received_data = json.loads(received_data)
-                # 송신 데이터 (수신된 json 에 따라 다르게 처리)
-                if 'hostname' in received_data.keys() and 'stage' in received_data.keys():
-                    send_data = {"score": [{"hostname": "host1", "stage": 99}]}
-                    connection.send(json.dumps(send_data).encode())
-            except json.decoder.JSONDecodeError:
+                i.send(pickle.dumps(send_data))
+            except Exception:
+                remove.append(i)
                 continue
+            for r in remove:
+                score_outgoing.remove(r)
 
 
 if __name__ == '__main__':
-    init_server()
+    MainServer(8080)
+    asyncore.loop()
+    client.close()
